7dd2a84fd3f32f05654d39501ae7237d6962f3f4
Mertcan Poyraz <mertcan_1999@icloud.com>
2023-03-18 00:38:01 +0300

    Add log to DefaultRestApi

--- src/main/java/com/enuygun/springbootelasticsearch/api/server/DefaultRestApi.java
+++ src/main/java/com/enuygun/springbootelasticsearch/api/server/DefaultRestApi.java
@@ -1,20 +1,22 @@
 package com.enuygun.springbootelasticsearch.api.server;
 
-import io.netty.util.internal.StringUtil;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.time.LocalTime;
 
 @RestController
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
 
+    private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
+
     @GetMapping(value = "/welcome")
     public String welcome() {
-        System.out.println(StringUtils.join("Hello", " this is" , " Spring Boot" , " REST API"));
+        LOG.info(StringUtils.join("Hello", " this is" , " Spring Boot" , " REST API"));
         return "Welcome to the Spring Boot";
     }
 
