d0263635ac274429bd6ead0c836f17735715c488
Mertcan Poyraz <mertcan_1999@icloud.com>
2023-03-18 02:45:18 +0300

    Add Post Request for Car Api

--- src/main/java/com/enuygun/springbootelasticsearch/api/server/CarApi.java
+++ src/main/java/com/enuygun/springbootelasticsearch/api/server/CarApi.java
@@ -3,15 +3,17 @@ package com.enuygun.springbootelasticsearch.api.server;
 import com.enuygun.springbootelasticsearch.entity.Car;
 import com.enuygun.springbootelasticsearch.service.CarService;
 import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Data
 @RequestMapping(value = "api/car/v1")
 @RestController
 public class CarApi {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CarApi.class);
 
     private final CarService carService;
 
@@ -20,4 +22,12 @@ public class CarApi {
     public Car random() {
        return carService.generateCar();
     }
+
+
+    @PostMapping(value = "/echo", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String echo(@RequestBody Car car) {
+        LOG.info("Car is {}", car);
+        return car.toString();
+    }
+
 }
