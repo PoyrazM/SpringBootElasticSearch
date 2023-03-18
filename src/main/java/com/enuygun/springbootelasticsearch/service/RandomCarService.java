4e01148279355f719c07d88735db41234f9c7c05
Mertcan Poyraz <mertcan_1999@icloud.com>
2023-03-18 17:14:00 +0300

    Car variables was changed

--- src/main/java/com/enuygun/springbootelasticsearch/service/RandomCarService.java
+++ src/main/java/com/enuygun/springbootelasticsearch/service/RandomCarService.java
@@ -1,6 +1,7 @@
 package com.enuygun.springbootelasticsearch.service;
 
 import com.enuygun.springbootelasticsearch.entity.Car;
+import com.enuygun.springbootelasticsearch.util.RandomDateUtil;
 import org.springframework.stereotype.Service;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -14,6 +15,15 @@ public class RandomCarService implements CarService{
         var color = COLORS.get(ThreadLocalRandom.current().nextInt(0, COLORS.size()));
         var types = TYPES.get(ThreadLocalRandom.current().nextInt(0, TYPES.size()));
 
-        return new Car(brand, color, types);
+        var available = ThreadLocalRandom.current().nextBoolean();
+        var price = ThreadLocalRandom.current().nextInt(5000, 12001);
+        var firstReleaseDate = RandomDateUtil.generateRandomLocalDate();
+
+        var result = new Car(brand, color, types);
+        result.setAvailable(available);
+        result.setPrice(price);
+        result.setFirstReleaseDate(firstReleaseDate);
+
+        return result;
     }
 }
