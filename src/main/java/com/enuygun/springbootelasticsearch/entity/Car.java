341d1272621ad462ffbc2929140f803bc7581d55
Mertcan Poyraz <mertcan_1999@icloud.com>
2023-03-18 17:03:12 +0300

    Add to 3 attributes in Car

--- src/main/java/com/enuygun/springbootelasticsearch/entity/Car.java
+++ src/main/java/com/enuygun/springbootelasticsearch/entity/Car.java
@@ -1,16 +1,25 @@
 package com.enuygun.springbootelasticsearch.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data @NoArgsConstructor @AllArgsConstructor
+import java.time.LocalDate;
+
+@Data @NoArgsConstructor @RequiredArgsConstructor
 public class Car {
 
+    @NonNull
     private String brand;
 
+    @NonNull
     private String color;
 
+    @NonNull
     private String type;
+
+    private int price;
+
+    private boolean available;
+
+    private LocalDate firstReleaseDate;
 
 }
